e7e859df2c37906ff1eea22253616e92c866ac88
bntnam <bntnam.app@gmail.com>
2018-12-15 23:01:38 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
+import registerServiceWorker from './registerServiceWorker';
+import firebase from './firebase';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  withRouter
+} from 'react-router-dom';
+import 'semantic-ui-css/semantic.min.css';
+import { createStore } from 'redux';
+import { Provider, connect } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './reducers';
+import { setUser } from './actions';
+import Spinner from './Spinner';
+
+const store = createStore(rootReducer, composeWithDevTools());
+
+class Root extends React.Component {
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.props.setUser(user);
+        this.props.history.push('/');
+      }
+    });
+  }
+
+  render() {
+    return this.props.isLoading ? (
+      <Spinner />
+    ) : (
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+      </Switch>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  isLoading: state.user.isLoading
+});
+
+const RootWithAuth = withRouter(
+  connect(
+    mapStateToProps,
+    { setUser }
+  )(Root)
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <RootWithAuth />
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+);
+registerServiceWorker();
---
+++ src/reducers/index.js
@@ -0,0 +1,25 @@
+import * as actionTypes from '../actions/types';
+import { combineReducers } from 'redux';
+
+const initialState = {
+  currentUser: null,
+  isLoading: true
+};
+
+const user_reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.SET_USER:
+      return {
+        currentUser: action.payload.currentUser,
+        isLoading: false
+      };
+    default:
+      return state;
+  }
+};
+
+const rootReducer = combineReducers({
+  user: user_reducer
+});
+
+export default rootReducer;
