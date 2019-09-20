b916985d5482c61e1dcbc30d18dc4f95cb5571a5
bntnam <bntnam.app@gmail.com>
2019-09-20 23:40:37 +0200

    Update

--- src/components/SidePanel/Channels.js
+++ src/components/SidePanel/Channels.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
-import { Menu, Icon, Modal, Form, Input, Button } from 'semantic-ui-react';
+import {
+  Menu,
+  Icon,
+  Modal,
+  Form,
+  Input,
+  Button,
+  Label
+} from 'semantic-ui-react';
 import { connect } from 'react-redux';
 
 import firebase from '../../firebase';
@@ -8,10 +16,13 @@ import { setCurrentChannel, setPrivateChannel } from '../../actions';
 class Channels extends Component {
   state = {
     user: this.props.currentUser,
+    channel: null,
     channels: [],
     channelName: '',
     channelDetails: '',
     channelsRef: firebase.database().ref('channels'),
+    messagesRef: firebase.database().ref('message'),
+    notifications: [],
     modal: false,
     firstLoad: true,
     activeChannel: ''
@@ -30,12 +41,55 @@ class Channels extends Component {
     this.state.channelsRef.on('child_added', snap => {
       loadedChannels.push(snap.val());
       this.setState({ channels: loadedChannels }, () => this.setFirstChannel());
+      this.addNotificationListener(snap.key);
     });
+  };
+
+  addNotificationListener = channelId => {
+    this.state.messagesRef.child(channelId).on('value', snap => {
+      if (this.state.channel) {
+        this.handleNotifications(
+          channelId,
+          this.state.channel.id,
+          this.state.notifications,
+          snap
+        );
+      }
+    });
+  };
+
+  handleNotifications = (channelId, currentChannelId, notifications, snap) => {
+    let lastTotal = 0;
+
+    let index = notifications.findIndex(
+      notification => notification.id === channelId
+    );
+
+    if (index !== -1) {
+      if (channelId !== currentChannelId) {
+        lastTotal = notifications[index].total;
+
+        if (snap.numChildren() - lastTotal > 0) {
+          notifications[index].count = snap.numChildren() - lastTotal;
+        }
+      }
+
+      notifications[index].lastKnownTotal = snap.numChildren();
+    } else {
+      notifications.push({
+        id: channelId,
+        total: snap.numChildren(),
+        lastKnownTotal: snap.numChildren(),
+        count: 0
+      });
+    }
+
+    this.setState({ notifications });
   };
 
   removeListeners = () => {
     this.state.channelsRef.off();
-  }
+  };
 
   addChannel = () => {
     const { channelsRef, channelName, channelDetails, user } = this.state;
@@ -81,21 +135,51 @@ class Channels extends Component {
 
   setActiveChannel = channel => {
     this.setState({ activeChannel: channel.id });
-  }
+  };
 
   setFirstChannel = () => {
     const firstChannel = this.state.channels[0];
     if (this.state.firstLoad && this.state.channels.length > 0) {
       this.props.setCurrentChannel(firstChannel);
       this.setActiveChannel(firstChannel);
+      this.setState({ channel: firstChannel });
     }
     this.setState({ firstLoad: false });
-  }
+  };
 
   changeChannel = channel => {
     this.setActiveChannel(channel);
+    this.clearNotification();
     this.props.setCurrentChannel(channel);
     this.props.setPrivateChannel(channel);
+    this.setState({ channel });
+  };
+
+  clearNotification = () => {
+    let index = this.state.notifications.findIndex(
+      notification => notification.id === this.state.channel.id
+    );
+
+    if (index !== -1) {
+      let updatedNotifications = [...this.state.notifications];
+      updatedNotifications[index].total = this.state.notifications[
+        index
+      ].lastKnownTotal;
+      updatedNotifications[index].count = 0;
+      this.setState({ notifications: updatedNotifications });
+    }
+  };
+
+  getNotificationCount = channel => {
+    let count = 0;
+
+    this.state.notifications.forEach(notification => {
+      if (notification.id === channel.id) {
+        count = notification.count;
+      }
+    });
+
+    if (count > 0) return count;
   };
 
   displayChannels = channels =>
@@ -108,6 +192,9 @@ class Channels extends Component {
         style={{ opacity: 0.7 }}
         active={channel.id === this.state.activeChannel}
       >
+        {this.getNotificationCount(channel) && (
+          <Label color='red'>{this.getNotificationCount(channel)}</Label>
+        )}
         # {channel.name}
       </Menu.Item>
     ));
@@ -121,12 +208,12 @@ class Channels extends Component {
 
     return (
       <React.Fragment>
-        <Menu.Menu className="menu">
+        <Menu.Menu className='menu'>
           <Menu.Item>
             <span>
-              <Icon name="exchange" /> CHANNELS
+              <Icon name='exchange' /> CHANNELS
             </span>{' '}
-            ({channels.length}) <Icon name="add" onClick={this.openModal} />
+            ({channels.length}) <Icon name='add' onClick={this.openModal} />
           </Menu.Item>
           {this.displayChannels(channels)}
         </Menu.Menu>
@@ -138,16 +225,16 @@ class Channels extends Component {
               <Form.Field>
                 <Input
                   fluid
-                  label="Name of Channel"
-                  name="channelName"
+                  label='Name of Channel'
+                  name='channelName'
                   onChange={this.handleChange}
                 />
               </Form.Field>
               <Form.Field>
                 <Input
                   fluid
-                  label="About the Channel"
-                  name="channelDetails"
+                  label='About the Channel'
+                  name='channelDetails'
                   onChange={this.handleChange}
                 />
               </Form.Field>
@@ -155,11 +242,11 @@ class Channels extends Component {
           </Modal.Content>
 
           <Modal.Actions>
-            <Button color="green" inverted onClick={this.handleSubmit}>
-              <Icon name="checkmark" /> Add
+            <Button color='green' inverted onClick={this.handleSubmit}>
+              <Icon name='checkmark' /> Add
             </Button>
-            <Button color="red" inverted onClick={this.closeModal}>
-              <Icon name="remove" /> Cancel
+            <Button color='red' inverted onClick={this.closeModal}>
+              <Icon name='remove' /> Cancel
             </Button>
           </Modal.Actions>
         </Modal>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -24,13 +24,13 @@ const user_reducer = (state = initialUserState, action) => {
   }
 };
 
-const initalChannelState = {
+const initialChannelState = {
   currentChannel: null,
   isPrivateChannel: false,
   userPosts: null
 };
 
-const channel_reducer = (state = initalChannelState, action) => {
+const channel_reducer = (state = initialChannelState, action) => {
   switch (action.type) {
     case actionTypes.SET_CURRENT_CHANNEL:
       return {
@@ -52,9 +52,27 @@ const channel_reducer = (state = initalChannelState, action) => {
   }
 };
 
+const initialColorsState = {
+  primaryColor: '#4c3c4c',
+  secondaryColor: '#eee'
+};
+
+const colors_reducer = (state = initialColorsState, action) => {
+  switch (action.type) {
+    case actionTypes.SET_COLORS:
+      return {
+        primaryColor: action.payload.primaryColor,
+        secondaryColor: action.payload.secondaryColor
+      };
+    default:
+      return state;
+  }
+};
+
 const rootReducer = combineReducers({
   user: user_reducer,
-  channel: channel_reducer
+  channel: channel_reducer,
+  colors: colors_reducer
 });
 
 export default rootReducer;
