a4b8ec4e0839793ae91bf2f583fd77664c1196c4
bntnam <bntnam.app@gmail.com>
2019-09-29 23:03:54 +0200

    Refactor

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from '@firebase/app';
+import '@firebase/auth';
+import '@firebase/database';
+import '@firebase/storage';
+
+var config = {
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
+};
+firebase.initializeApp(config);
+
+export default firebase;
